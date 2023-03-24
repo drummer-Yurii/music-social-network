8c302444c6d69998ff8110c03b266032ed116e5b
Yurii Rybachok <drummer2009@ukr.net>
2023-03-24 16:30:01 +0200

    update profile image

--- src/views/account/EditProfile.vue
+++ src/views/account/EditProfile.vue
@@ -2,77 +2,48 @@
     <div id="EditProfile" class="container max-w-4xl mx-auto pt-20 pb-20 px-6">
         <div class="text-gray-900 text-xl">Edit Profile</div>
         <div class="bg-green-500 w-full h-1"></div>
-        <CropperModal 
-            v-if="showModal"
-            :minAspectRatioProp="{width: 8, height: 8}"
-            :maxAspectRatioProp="{width: 8, height: 8}"
-            @croppedImageData="setCroppedImageData"
-            @showModal="showModal = false"
-        />
+        <CropperModal v-if="showModal" :minAspectRatioProp="{ width: 8, height: 8 }"
+            :maxAspectRatioProp="{ width: 8, height: 8 }" @croppedImageData="setCroppedImageData"
+            @showModal="showModal = false" />
         {{ image }}
         <div class="flex flex-wrap mt-4 mb-6">
             <div class="w-full md:w-1/2 px-3">
-                <TextInput 
-                    label="First Name"
-                    placeholder="John"
-                    v-model:input="firstName"
-                    inputType="text"
-                    :error="errors.first_name ? errors.first_name[0] : ''"
-                />
+                <TextInput label="First Name" placeholder="John" v-model:input="firstName" inputType="text"
+                    :error="errors.first_name ? errors.first_name[0] : ''" />
             </div>
             <div class="w-full md:w-1/2 px-3">
-                <TextInput 
-                    label="Last Name"
-                    placeholder="Doe"
-                    v-model:input="lastName"
-                    inputType="text"
-                    :error="errors.last_name ? errors.last_name[0] : ''"
-                />
+                <TextInput label="Last Name" placeholder="Doe" v-model:input="lastName" inputType="text"
+                    :error="errors.last_name ? errors.last_name[0] : ''" />
             </div>
         </div>
         <div class="flex flex-wrap mt-4 mb-6">
             <div class="w-full md:w-1/2 px-3">
-                <TextInput 
-                    label="Location"
-                    placeholder="Madrid, ES"
-                    v-model:input="location"
-                    inputType="text"
-                />
-            </div>  
+                <TextInput label="Location" placeholder="Madrid, ES" v-model:input="location" inputType="text" />
+            </div>
         </div>
         <div class="flex flex-wrap mt-4 mb-6">
             <div class="w-full md:w-1/2 px-3">
-               <DisplayCropperButton 
-                    label="Profile Image"
-                    btnText="Update Profile Image"
-                    @showModal="showModal = true"
-               />
-            </div>  
+                <DisplayCropperButton label="Profile Image" btnText="Update Profile Image" @showModal="showModal = true" />
+            </div>
         </div>
         <div class="flex flex-wrap mt-4 mb-6">
             <div class="w-full md:w-1/2 px-3">
-               <CroppedImage 
-                    label="Cropped Image"
-                    :image="image"
-               />
-            </div>  
+                <CroppedImage 
+                    label="Cropped Image" 
+                    :image="'http://127.0.0.1:8001/images/users/' + image" />
+            </div>
         </div>
+        {{ 'http://127.0.0.1:8001/images/users/' + image }}
         <div class="flex flex-wrap mt-4 mb-6">
             <div class="w-full px-3">
-               <TextArea
-                    label="Description"
-                    placeholder="Please enter some information here!!!"
-                    v-model:description="description"
-               />
-            </div>  
+                <TextArea label="Description" placeholder="Please enter some information here!!!"
+                    v-model:description="description" />
+            </div>
         </div>
         <div class="flex flex-wrap mt-8 mb-6">
             <div class="w-full px-3">
-               <SubmitFormButton 
-                    btnText="Update Profile"
-                    @click="updateUser"
-               />
-            </div>  
+                <SubmitFormButton btnText="Update Profile" @click="updateUser" />
+            </div>
         </div>
     </div>
 </template>
@@ -97,7 +68,7 @@ let firstName = ref(null);
 let lastName = ref(null);
 let location = ref(null);
 let description = ref(null);
-// let imageData = null;
+let imageData = null;
 let image = ref(null);
 let errors = ref([]);
 
@@ -110,7 +81,7 @@ onMounted(() => {
 })
 
 const setCroppedImageData = (data) => {
-    // imageData = data
+    imageData = data
     image.value = data.imageUrl
 }
 
@@ -123,6 +94,14 @@ const updateUser = async () => {
     data.append('last_name', lastName.value || '')
     data.append('location', location.value || '')
     data.append('description', description.value || '')
+
+    if (imageData) {
+        data.append('image', imageData.file || '')
+        data.append('height', imageData.height || '')
+        data.append('width', imageData.width || '')
+        data.append('left', imageData.left || '')
+        data.append('top', imageData.top || '')
+    }
 
     try {
         await axios.post('users/' + userStore.id + '?_method=PUT', data)
